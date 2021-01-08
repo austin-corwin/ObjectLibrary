87715ed4928aa0cc40e297df5da07ddc934b8db8
Austin Corwin <austincorwin5@gmail.com>
2021-01-08 10:45:05 -0700

    Built out JS functionality

--- main.js
+++ main.js
@@ -1,6 +1,9 @@
 
 
 const myLibrary = [];
+const saveButton = document.querySelector('.modal-save');
+
+
 
 function book(name,author,pages,read){
     this.name = name;
@@ -23,55 +26,63 @@ function addBookToLibrary(book){
    
 };
 
-addBookToLibrary(theHobbit);
-addBookToLibrary(wereNone);
-addBookToLibrary(the92);
 
 
+// addBookToLibrary(theHobbit);
+// addBookToLibrary(wereNone);
+// addBookToLibrary(the92);
+// addBookToLibrary(huckleberryFinn);
 
-myLibrary.forEach(function (book) {
-    let bookName = book.name;
-    let bookAuthor = book.author;
-    let bookPages = book.pages;
-    let bookReadStatus = book.read;
 
-    //create a div for the new book
-
-    let newBookDiv = document.createElement('div');
-    let bookRow = document.querySelector('.book-row');
-    bookRow.appendChild(newBookDiv);
-    newBookDiv.setAttribute('class','col border book')
-
-    // create a div for the book name
-    let newBookNameDiv = document.createElement('div');
-    newBookDiv.appendChild(newBookNameDiv);
-    newBookNameDiv.setAttribute('class','book-name');
-    newBookNameDiv.textContent = `Name: ${bookName}`;
+function populateBook(book){
+    myLibrary.forEach(function (book) {
+        let bookName = book.name;
+        let bookAuthor = book.author;
+        let bookPages = book.pages;
+        let bookReadStatus = book.read;
     
-    //create a div for the book author
-    let newBookAuthorDiv = document.createElement('div');
-    newBookDiv.appendChild(newBookAuthorDiv);
-    newBookAuthorDiv.setAttribute('class','book-author');
-    newBookAuthorDiv.textContent = `Author: ${bookAuthor}`;
-
-    //create a div for the book page number
-    let newBookPagesDiv = document.createElement('div');
-    newBookDiv.appendChild(newBookPagesDiv);
-    newBookPagesDiv.setAttribute('class','book-pages');
-    newBookPagesDiv.textContent = `Pages: ${bookPages}`;
+        //create a div for the new book
     
-    //create a div to show if you've read the book
-    let newBookReadStatusDiv = document.createElement('div');
-    newBookDiv.appendChild(newBookReadStatusDiv);
-    newBookReadStatusDiv.setAttribute('class','book-pages');
-    newBookReadStatusDiv.textContent = `Have you read it? ${bookReadStatus}`;
+        let newBookDiv = document.createElement('div');
+        let bookRow = document.querySelector('.book-row');
+        bookRow.appendChild(newBookDiv);
+        newBookDiv.setAttribute('class','col-12 col-md-4 p-2 rounded border book')
+    
+        // create a div for the book name
+        let newBookNameDiv = document.createElement('div');
+        newBookDiv.appendChild(newBookNameDiv);
+        newBookNameDiv.setAttribute('class','book-name');
+        newBookNameDiv.textContent = `Name: ${bookName}`;
+        
+        //create a div for the book author
+        let newBookAuthorDiv = document.createElement('div');
+        newBookDiv.appendChild(newBookAuthorDiv);
+        newBookAuthorDiv.setAttribute('class','book-author');
+        newBookAuthorDiv.textContent = `Author: ${bookAuthor}`;
+    
+        //create a div for the book page number
+        let newBookPagesDiv = document.createElement('div');
+        newBookDiv.appendChild(newBookPagesDiv);
+        newBookPagesDiv.setAttribute('class','book-pages');
+        newBookPagesDiv.textContent = `Pages: ${bookPages}`;
+        
+        //create a div to show if you've read the book
+        let newBookReadStatusDiv = document.createElement('div');
+        newBookDiv.appendChild(newBookReadStatusDiv);
+        newBookReadStatusDiv.setAttribute('class','book-pages');
+        newBookReadStatusDiv.textContent = `Have you read it? ${bookReadStatus}`;
+
+        
+    
+    
+        console.log(bookName);
+        console.log(bookAuthor);
+        console.log(bookPages);
+    
+    });
+}
 
 
-    console.log(bookName);
-    console.log(bookAuthor);
-    console.log(bookPages);
-
-});
 
 
 
@@ -81,14 +92,41 @@ console.log(myLibrary);
 
 // The Modal Javascript //
 
-var myModal = document.getElementById('myModal')
-var myInput = document.getElementById('myInput')
+let bookModal = document.getElementById('bookModal')
+let myInput = document.getElementById('myInput')
 
-myModal.addEventListener('shown.bs.modal', function () {
+bookModal.addEventListener('shown.bs.modal', function () {
   myInput.focus()
 })
 
 // End Modal Javascript //
+
+function saveChanges(name,author,pages,read){
+    let titleValue = document.querySelector('.title-input').value;
+    let authorValue = document.querySelector('.author-input').value;
+    let pagesValue = document.querySelector('.pages-input').value;
+    let readValue = document.querySelector('.read-input').value;
+    let newBookSubmission = new book(titleValue,authorValue,pagesValue,readValue);
+   addBookToLibrary(newBookSubmission);
+   console.log(myLibrary);
+   console.log(titleValue);
+   console.log(authorValue);
+   console.log(pagesValue);
+   console.log(readValue);
+
+   populateBook(newBookSubmission);
+
+    
+}
+
+saveButton.addEventListener('click', saveChanges);
+
+
+
+
+
+
+
 
 //Loop through the array and display books to the page
 
